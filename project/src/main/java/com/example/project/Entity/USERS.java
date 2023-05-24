aab2327a14aa80b49422f66f95efd0482448c3d5
lee-410 <58701102+lee-410@users.noreply.github.com>
2023-05-24 12:04:19 +0900

    엔티티 추가 작성

--- project/src/main/java/com/example/project/Entity/USERS.java
+++ project/src/main/java/com/example/project/Entity/USERS.java
@@ -3,6 +3,8 @@ package com.example.project.Entity;
 import jakarta.persistence.*;
 import lombok.*;
 
+import java.time.LocalDateTime;
+
 @ToString
 @Getter
 @Builder
@@ -12,10 +14,23 @@ import lombok.*;
 @Entity
 public class USERS {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+    @Id // PK
+    @GeneratedValue(strategy = GenerationType.IDENTITY) //1씩 자동증가
+    private Long user_no;
+
+    @Column(length = 200, nullable = false) //NOT NULL, 200글자 제한(string에서만)
+    private String user_id;
 
     @Column(length = 200, nullable = false)
-    private String usersText;
+    private String user_pw;
+
+    @Column(length = 200, nullable = false)
+    private String user_name;
+
+    @Column(length = 200, nullable = false)
+    private String user_auth;
+
+    @Column
+    private LocalDateTime append_date;
+
 }
