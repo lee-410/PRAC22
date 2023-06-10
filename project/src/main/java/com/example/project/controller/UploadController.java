6ea00cd0385b7cc99949d7829015420fd2e9854d
lee-410 <58701102+lee-410@users.noreply.github.com>
2023-06-10 15:16:21 +0900

    upload 수정
    실패할 가능성이 있기 때문에 upload.html은 놔두고, 아직 안쓰고 있는 profile.html에 test를 진행했다.
    
    profile에서 레이아웃을 바꾸고 런을 했는데 오류가 남-> 이미지 post하는 과정에서 upload.html과 겹치는 부분이 있어서?(폴더에 이미지도 없었음) upload.html을 모두 주석처리해서 해결
    
    table td안에 이미지를 나란히 정렬해놓고 싶어서 colspan을 10까지 해놓았는데 이미지 하나 업로드했더니 10칸모두 이미지가 업로드가 됨 -> td 하나안에 이미지를 나란히 하는것으로 변경
    
    이미지 2개를 업로드하니까 이미지가 위아래로 정렬이 됨. 이미지 업로드 하기 전 <div>안에 class=test를 해서 float:left를 함으로써 해결.
    
    <div class="test"> 404에러
    자바스크립트안에서 html을 선언한것이기 때문에 "는 \"로 해야함-> 해결

--- project/src/main/java/com/example/project/controller/UploadController.java
+++ project/src/main/java/com/example/project/controller/UploadController.java
@@ -68,7 +68,7 @@ public class UploadController {
 
                 File thumbnailFile = new File(thubmnailSaveName);
 
-                Thumbnailator.createThumbnail(savePath.toFile(),thumbnailFile,250,250);// 섬네일 생성
+                Thumbnailator.createThumbnail(savePath.toFile(),thumbnailFile,40,40);// 섬네일 생성
 
                 resultDTOList.add(new UploadResultDTO(fileName,uuid,folderPath));
             }catch (IOException e){
