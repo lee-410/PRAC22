3e5897ad6daa88cbe48fa56ffcfde1dcb92fcbc8
lee-410 <58701102+lee-410@users.noreply.github.com>
2023-06-20 00:30:44 +0900

    security token 해결
    십팔 gpt 멍청한 새기가 로그인할때 csrf랑은 상관없다고 했는데 존나 상관있었다 개열받네진짜

--- project/src/main/java/com/example/project/config/SpringSecurityConfig.java
+++ project/src/main/java/com/example/project/config/SpringSecurityConfig.java
@@ -1,11 +1,20 @@
 package com.example.project.config;
 
 import jakarta.servlet.DispatcherType;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.AuthenticationFailureHandler;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+
+import java.io.IOException;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
@@ -24,7 +33,7 @@ public class SpringSecurityConfig {
                 .formLogin(login -> login
                         .loginPage("/view/login")
                         .loginProcessingUrl("/login-process")
-                        .defaultSuccessUrl("/view/dashboard", true)
+                        .defaultSuccessUrl("/", true)
                         .failureUrl("/login.html?error=true")
                         .permitAll()
                 )
